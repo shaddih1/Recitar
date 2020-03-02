431b18e8beaa6b1d466c2c8520eb1ed87adf5f55
Shaddi H <shaddih1@gmail.com>
2020-03-01 21:05:17 -0600

    Create basic.sh

---
+++ basic.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# functions for bug bounty
+function mkb(){
+  mkdir {InfoGathering, Mapping, Discovery, Explotation, POCs}
+}
+
+function openPorts(){
+  tput civis
+  for port in {seq 1 65535}; do
+    timeout 1 bash -c 'echo "" < /dev/tcp/$1/$port' 2>/dev/null && 'Port [ $port ] - [Open]'
+  done
+  wait
+  tput cnorm
+}
